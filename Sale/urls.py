a047e474297461087f1ff3ac95ff6e8cc88d52b3
kibbae <kristbjorge21@ru.is>
2022-05-04 12:16:18 +0000

    cart app

--- Sale/urls.py
+++ Sale/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', include('FireSale.urls')),
     path('user', include('user.urls')),
-    path('product', include('product.urls'))
+    path('product', include('product.urls')),
+    path('cart', include('cart.urls'))
 
 ]
