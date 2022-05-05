6653d1d6bed3d37c4a63a8694e8446c1edc4e272
kibbae <kristbjorge21@ru.is>
2022-05-05 12:39:25 +0000

    database

--- Sale/settings.py
+++ Sale/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Sale.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'firesale-db',
+        'USER': 'firesale',
+        'PASSWORD': 'fireandsale',
+        'HOST': '34.66.64.23',
+        'PORT': '5432'
     }
 }
 
