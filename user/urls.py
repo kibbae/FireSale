aa0a2f1b790e8ce4b0e797446dbb943ffafb6e89
Perla Steingrimsdottir <perlasteingrimsdottir@Perlas-MacBook-2.local>
2022-05-05 13:08:28 +0000

    login/register

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="user"),
+    path('', views.index, name='user'),
+    path('register', views.register, name='register'),
+    path('login', LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout', Logoutview.as_view(next_page='login'), name='logout')
 ]
--- user/views.py
+++ user/views.py
@@ -7,3 +7,13 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'user/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    return render(request, 'user/register.html', {
+        'form': UserCreationForm()
+    })
