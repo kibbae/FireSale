7099891bea28e40103dccef1743f921cfb1e21f3
Perla Steingrimsdottir <perlasteingrimsdottir@Perlas-MacBook-2.local>
2022-05-06 15:02:00 +0000

    meira login/register á navbar

--- FireSale/views.py
+++ FireSale/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
+
+# Til að sjá ekki ef ekki loggaður inn
+# @login_required
+# def blabla():
+# pass
--- user/views.py
+++ user/views.py
@@ -2,13 +2,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 
-#user = [
+# user = [
 #    {'name': 'Kristbjörg', 'kristbjorge21@ru.is'}
-#]
+# ]
 
 # Create your views here.
 def index(request):
     return render(request, 'user/index.html')
+
 
 def register(request):
     if request.method == 'POST':
