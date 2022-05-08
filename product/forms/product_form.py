1a8f1bf9e42a8eaa90324767d2e5e80774c7a670
Sophie <sophie_cariven@hotmail.fr>
2022-05-08 22:08:48 +0000

    products form + create update html  delete video 9

---
+++ product/forms/product_form.py
@@ -0,0 +1,42 @@
+from django.forms import ModelForm, widgets
+from django import forms
+from product.models import Product
+# from offer.models import Offer
+
+# video 9
+
+
+class ProductCreateForm(ModelForm):
+    image = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
+
+    class Meta:
+        model = Product
+        exclude = ['id', 'highest_offer', 'available', 'seller']
+        widgets = {
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'condition': widgets.TextInput(attrs={'class': 'form-control'}),
+            'description': widgets.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+class ProductUpdateForm(ModelForm):
+    class Meta:
+        model = Product
+        exclude = ['id', 'highest_offer', 'available', 'seller']
+        widgets = {
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'condition': widgets.TextInput(attrs={'class': 'form-control'}),
+            'description': widgets.TextInput(attrs={'class': 'form-control'}),
+        }
+
+#
+# class MakeOfferForm(ModelForm):
+#
+#     class Meta:
+#         model = Offer
+#         exclude = ['id', 'accepted', 'buyer', 'item']
+#         widgets = {
+#             'price': widgets.TextInput(attrs={'class': 'form-control'}),
+#             'message': widgets.TextInput(attrs={'class': 'form-control'})
+#         }
+
--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name="product"),
     path('<int:id>', views.get_product_by_id, name="product-details"),
+    path('create_product', views.create_product, name="create-product"), # video 9
+    path('delete_item/<int:id>', views.delete_product, name='delete-item'),
+    # path('update_item/<int:id>', views.update_product, name='update-item'),
+    # path('sort_item', views.sort_product, name="sort-item"),
+    # path('make_offer/<int:id>', views.make_offer, name="make-offer")
+
 ]
