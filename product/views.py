fb85d64dd4f5b78d92891b2383fe4cf27fe2d0a8
Sophie <sophie_cariven@hotmail.fr>
2022-05-10 10:58:27 +0000

    trying to fix get_by_id, only product/views.py changed, waiting for login changes

--- product/views.py
+++ product/views.py
@@ -5,12 +5,13 @@ from django.forms import ModelForm, widgets
 from django import forms
 
 
-# Create your views here.
+# /products
 def index(request, products='product'):
     context = {'products': Product.objects.all().order_by('name')}
     return render(request, 'product/index.html', context={'products': products})
 
 
+# products/3
 def get_product_by_id(request, id):
     return render(request, 'product/product_details.html', {
         'product': get_object_or_404(Product, pk=id)
@@ -18,6 +19,7 @@ def get_product_by_id(request, id):
 
 
 # video 9
+# products/create_product
 def create_product(request):
     if request.method == 'POST':
         form= ProductCreateForm(data=request.POST)
@@ -33,12 +35,15 @@ def create_product(request):
     })
 
 
+# products/delete_product/4
 def delete_product(request, id):
     product = get_object_or_404(Product, pk=id)
     product.delete()
     return redirect('products') # need to redirect to the name '' registered in the views
+# not a requirement, so let's see with that
 
 
+# products/update_product/4
 def update_product(request, id):
     instance = get_object_or_404(Product, pk=id)
     if request.method == 'POST':
