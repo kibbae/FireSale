bc5bbca0b8b433cf82ee60722c8747a4cadd877e
Sophie <sophie_cariven@hotmail.fr>
2022-05-11 22:52:23 +0000

    trying to resolve merge conflict branch sophie117

--- product/forms/product_form.py
+++ product/forms/product_form.py
@@ -1,10 +1,9 @@
 from django.forms import ModelForm, widgets
 from django import forms
 from product.models import Product
-# from offer.models import Offer
+
 
 # video 9
-
 
 class ProductCreateForm(ModelForm):
     image = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
@@ -33,14 +32,13 @@ class ProductUpdateForm(ModelForm):
             'category': widgets.Select(attrs={'class': 'form-control'}),
         }
 
-#
-# class MakeOfferForm(ModelForm):
-#
-#     class Meta:
-#         model = Offer
-#         exclude = ['id', 'accepted', 'buyer', 'item']
-#         widgets = {
-#             'price': widgets.TextInput(attrs={'class': 'form-control'}),
-#             'message': widgets.TextInput(attrs={'class': 'form-control'})
-#         }
+
+class MakeOfferForm(ModelForm):
+
+    class Meta:
+        model = Product
+        exclude = ['id', 'item', 'buyer', 'timestamp', 'is_accepted']
+        widgets = {
+            'price': widgets.TextInput(attrs={'class': 'form-control'})
+        }
 
