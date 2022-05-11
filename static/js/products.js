650fd754b9b1931146e245ba706d7e069630a1ba
kibbae <kristbjorge21@ru.is>
2022-05-11 14:41:45 +0000

    laga leit og tengja user við product

--- static/js/products.js
+++ static/js/products.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
-    $('#search-btn').on( types: 'click', selector: function(e) {
-        e.preventDefault():
+    $('#search-btn').on('click', function(e) {
+        e.preventDefault()
+        console.log('Hallo')
         var searchText = $('#search-box').val()
-        $.ajax( url: {
-            url: '/products?search_filter' + searchText,
+        $.ajax(  {
+            url: '/products?search_filter=' + searchText,
                 type: 'GET',
                 success: function(resp) {
-                    var newHtml = resp.data.map(d => {
-                        return <div class="well product">
+                console.log(resp.data)
+                var newHtml = resp.data.map(d => {
+                        return `<div class="well product">
                                     <a href="/products/${d.id}">
                                         <img class="product-img" src="${d.firstImage}" />
                                         <h4>${d.name}</h4>
                                         <p>${d.description}</p>
                                     </a>
-                                </div>
+                                </div>`
                     });
                     $('.products').html(newHtml.join(''));
-                    $('search-box').val( value: '');
+                    $('search-box').val( '');
             },
             error: function(xhr, status, error) {
                 console.error(error);
