db360129eca40d7fa688b01a49d2aa217e01f6f5
kibbae <kristbjorge21@ru.is>
2022-05-12 12:25:45 +0000

    offer working

--- product/urls.py
+++ product/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('create_product', views.create_product, name="create-product"), # video 9
     path('update_product/<int:id>', views.update_product, name='update-product'),
     path('delete_product/<int:id>', views.delete_product, name='delete-product'),
-    path('make_offer/<int:id>', views.make_offer, name="make-offer")
+    path('make_offer/<int:id>', views.get_product_by_id, name="make-offer")
 ]
