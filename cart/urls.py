1c6f69a199720a3d95333f1d0b23488ad868b2bd
kibbae <kristbjorge21@ru.is>
2022-05-12 15:35:30 +0000

    user getur bara deletað hans vörum ekki öllum

--- cart/urls.py
+++ cart/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="cart"),
     path('payment', views.payment, name="payment"),
-    path('shipping', views.shipping, name="shipping"),
+    #path('shipping', views.shipping, name="shipping"),
     path('orderreview', views.orderreviw, name="orderreview")
 ]
