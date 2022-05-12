9f02762fb2d99b91a717d51b77e6ae1e558e0ffa
Perla Steingrimsdottir <perlasteingrimsdottir@Perlas-MacBook-2.local>
2022-05-12 10:07:01 +0000

    order by, name, price

--- FireSale/views.py
+++ FireSale/views.py
@@ -1,14 +1,33 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render
 from product.models import Product, ProductImage
 
 
-# Create your views here.
 def index(request):
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
+        products = [{
+            'id': x.id,
+            'name': x.name,
+            'description': x.description,
+            'firstImage': x.productimage_set.first().image
+        } for x in Product.objects.filter(name__icontains=search_filter)]
+        # products = list(Product.objects.filter(name__icontains=search_filter).values())
+        return JsonResponse({'data': products})
+    if 'order_by' in request.GET:
+        split = request.GET['order_by'].split("_")
+        field = split[0]
+        order = split[1]
+        if order == "asc":
+            order = ""
+        else:
+            order = "-"
+        print(order + field)
+        context = {'products': Product.objects.all().order_by(order + field)}
+        return render(request, 'product/index.html', context)
     context = {'products': Product.objects.all().order_by('name')}
-    return render(request, 'product/index.html', context=context)
-
-
+    return render(request, 'product/index.html', context)
 
 # Til að sjá ekki ef ekki loggaður inn
 # @login_required
